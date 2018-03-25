24a4dd4366e4604bb37cab4a3c3106814fa7c537
DATAGEARBI\Mohamed.Farag <mohamed.farag@datagearbi.com>
2018-03-25 10:39:17 +0300

    add Suspected Controller

--- src/main/java/com/datagearbi/controller/SuspectedController.java
+++ src/main/java/com/datagearbi/controller/SuspectedController.java
@@ -2,8 +2,6 @@ package com.datagearbi.controller;
 
 import java.util.List;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
