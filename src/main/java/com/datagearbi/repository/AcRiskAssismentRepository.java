0a5635935f03c3cb921f008e7c1fb230ce862803
Ahmed Shalash <ashalash38@gmail.com>
2018-05-17 16:33:53 +0200

    RiskAssisment  commit

---
+++ src/main/java/com/datagearbi/repository/AcRiskAssismentRepository.java
@@ -0,0 +1,10 @@
+package com.datagearbi.repository;
+
+import com.datagearbi.model.AcRiskAssessment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by USER-5 on 17/05/2018.
+ */
+public interface AcRiskAssismentRepository extends JpaRepository<AcRiskAssessment,Long> {
+}
