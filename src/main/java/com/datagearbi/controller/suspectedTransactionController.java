d8c8361fe069c52662e7856833fb46f2fb91cd7f
hussienalbared <hussienalbared@gmail.com>
2018-07-22 11:49:28 +0200

    fix bug in suspected transaction

--- src/main/java/com/datagearbi/controller/suspectedTransactionController.java
+++ src/main/java/com/datagearbi/controller/suspectedTransactionController.java
@@ -26,14 +26,20 @@ public class suspectedTransactionController {
 	private suspected_transactions_VRepository suspected_transactions_VRepository;
 	
 	@RequestMapping("all")
-	public List  suspectedTransaction(@RequestParam("partyNumber")String partyNumber) {
+	public List<Suspected_transactions_V>  suspectedTransaction(@RequestParam("partyNumber")String partyNumber) {
 	
-		List<Integer> accountKeys1=this.entityManager.createQuery("select B.id.acct_Key from Customer_X_Account B where B.chg_Current_Ind='Y'   and B.id.cust_Key="+partyNumber).getResultList();
-		Query q1=this.entityManager.createQuery("select  A.acct_Key from Account A where A.chg_Current_Ind='Y' and  A.acct_Key in (:varList)");
+		@SuppressWarnings("unchecked")
+		List<Integer> accountKeys1=this.entityManager.createQuery("select B.id.acct_Key from Customer_X_Account B where B.chg_Current_Ind='Y'   and B.cust_No='"+partyNumber+"'").getResultList();
+		if(accountKeys1.size()<1)
+			return null;
+		Query q1=this.entityManager.createQuery("select  A.acct_Key from Account A where A.chg_Current_Ind='Y' and  A.acct_Key in :varList");
 		q1.setParameter("varList", accountKeys1);
 
 		List<Integer> accountKeys2=	q1.getResultList();
-		Query q2=entityManager.createQuery("select s from Suspected_transactions_V s where s.acct_Key in (:varList2)");
+		if(accountKeys2.size()<1)
+			return null;
+
+		Query q2=entityManager.createQuery("select s from Suspected_transactions_V s where s.acct_Key in :varList2");
 		q2.setParameter("varList2", accountKeys2);
 		return q2.getResultList();
 		
