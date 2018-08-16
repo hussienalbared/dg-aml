24216904aa3433fead99bf9cc3aad88b06a9b61a
Amr_Almajeedi <amabedu0@gmail.com>
2018-08-16 13:26:47 +0200

    scenario parameters

---
+++ src/main/java/com/datagearbi/helper/Key_ValueHelper.java
@@ -0,0 +1,34 @@
+package com.datagearbi.helper;
+
+import java.io.Serializable;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonValue;
+
+//@JsonFormat(shape=JsonFormat.Shape.ARRAY)
+public class Key_ValueHelper implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	 @JsonProperty("param_Name")
+	private String param_Name;
+	 @JsonProperty("param_Value")
+	private String param_Value;
+	public String getParam_Name() {
+		return param_Name;
+	}
+	public void setParam_Name(String param_Name) {
+		this.param_Name = param_Name;
+	}
+	public String getParam_Value() {
+		return param_Value;
+	}
+	public void setParam_Value(String param_Value) {
+		this.param_Value = param_Value;
+	}
+	
+	public Key_ValueHelper() {
+		
+	}
+	
+}
