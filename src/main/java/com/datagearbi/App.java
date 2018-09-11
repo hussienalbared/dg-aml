4456d84dea38eaf39349bd97b2aea2ed97429807
Mohammed Farag <mohamed.farag@datagearbi.com>
2018-09-11 13:37:12 +0200

    update like spring boot 2.0.4

--- src/main/java/com/datagearbi/App.java
+++ src/main/java/com/datagearbi/App.java
@@ -1,16 +1,19 @@
 package com.datagearbi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 import com.datagearbi.helper.FileStorageProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
     FileStorageProperties.class
 })
-public class App extends SpringBootServletInitializer{
+@EnableAutoConfiguration(exclude=DataSourceAutoConfiguration.class)
+public class App {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
